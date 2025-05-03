4fd5783b31075179f071848e61ca1e232eb70ffe
Josep Viciana <emmgfx@gmail.com>
2025-05-03 16:57:45 +0200

    part1, exercise 8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,9 +6,6 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const total = good + neutral + bad;
-  const average = ((good * 1) + (neutral * 0) + (bad * -1) / total) || 0;
-  const positive = good * 100 / total || 0;
 
   const increaseGood = () => setGood(good + 1)
   const increaseNeutral = () => setNeutral(neutral + 1)
@@ -20,6 +17,18 @@ const App = () => {
       <button onClick={increaseGood}>Good</button>
       <button onClick={increaseNeutral}>Neutral</button>
       <button onClick={increaseBad}>Bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  const average = ((good * 1) + (neutral * 0) + (bad * -1) / total) || 0;
+  const positive = good * 100 / total || 0;
+
+  return (
+    <>
       <h1>Statistics</h1>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
@@ -27,8 +36,9 @@ const App = () => {
       <p>All: {total}</p>
       <p>Average: {average}</p>
       <p>Positive: {positive} %</p>
-    </div>
+    </>
   )
 }
+
 
 export default App
