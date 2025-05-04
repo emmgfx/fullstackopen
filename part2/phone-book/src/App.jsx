df5f5d30af8d86d43782eff3856ec3d6b74769da
Josep Viciana <emmgfx@gmail.com>
2025-05-04 11:23:26 +0200

    part2, exercise 8

--- part2/phone-book/src/App.jsx
+++ part2/phone-book/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '000-000-000' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleAdd = (event) => {
     event.preventDefault();
@@ -13,17 +14,21 @@ const App = () => {
       alert(`${newName} is already added to phonebook`);
       return;
     }
-    
-    setPersons([...persons, { name: newName }]);
+
+    setPersons([...persons, { name: newName, number: newNumber }]);
     setNewName("");
+    setNewNumber("");
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleAdd}>
-        <div>
+      <div>
           name: <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -31,7 +36,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => {
-        return <p key={person.name}>{person.name}</p>
+        return <p key={person.name}>{person.name} {person.number}</p>
       })}
     </div>
   )
