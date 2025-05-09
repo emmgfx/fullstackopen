8650dff0bcb89d76a221852a53e076456af66780
Josep Viciana <emmgfx@gmail.com>
2025-05-09 18:46:52 +0200

    part3, exercise 8

--- part3/api/index.js
+++ part3/api/index.js
@@ -2,9 +2,23 @@ const express = require("express");
 var morgan = require("morgan");
 
 const app = express();
+morgan.token("body", (req) => JSON.stringify(req.body));
 
 app.use(express.json());
-app.use(morgan("default"));
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens["body"](req, res),
+    ].join(" ");
+  })
+);
 
 let persons = [
   {
