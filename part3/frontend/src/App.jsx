8c4d7d7f95bad8bf29ff8421107d84647e1faee6
Josep Viciana <emmgfx@gmail.com>
2025-05-11 12:40:05 +0200

    part3, exercise 19

--- part3/frontend/src/App.jsx
+++ part3/frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 import personsService from "./personService";
 
 import { Persons } from "./components/Persons";
 import { PersonForm } from "./components/PersonForm";
 import { FilterForm } from "./components/FilterForm";
-import { Notification } from './components/Notification';
+import { Notification } from "./components/Notification";
 
 const App = () => {
-  const [filter, setFilter] = useState('');
-  const [newName, setNewName] = useState('');
-  const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState("");
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
   const [persons, setPersons] = useState([]);
   const [successMessage, setSuccessMessage] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
@@ -22,59 +22,81 @@ const App = () => {
   const notification = (type, message) => {
     setSuccessMessage(null);
     setErrorMessage(null);
-    
-    if(type === "success"){
+
+    if (type === "success") {
       setSuccessMessage(message);
-      setTimeout(() => { setSuccessMessage(null); }, 5000);
-    }else if(type === "error"){
+      setTimeout(() => {
+        setSuccessMessage(null);
+      }, 5000);
+    } else if (type === "error") {
       setErrorMessage(message);
-      setTimeout(() => { setErrorMessage(null); }, 5000);
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
     }
-  }
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
 
     const existingPerson = persons.find((person) => person.name === newName);
 
-    if(existingPerson){
-      if(window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
-        personsService.update(existingPerson.id, { name: newName, number: newNumber }).then((updatedPerson) => {
-          const newPersonsArray = persons.map((person) => {
-            return person.id === updatedPerson.id
-              ? updatedPerson
-              : person;
+    if (existingPerson) {
+      if (
+        window.confirm(
+          `${newName} is already added to phonebook, replace the old number with a new one?`
+        )
+      ) {
+        personsService
+          .update(existingPerson.id, { name: newName, number: newNumber })
+          .then((updatedPerson) => {
+            const newPersonsArray = persons.map((person) => {
+              return person.id === updatedPerson.id ? updatedPerson : person;
+            });
+            setPersons(newPersonsArray);
+            setNewName("");
+            setNewNumber("");
+            notification("success", `Updated ${updatedPerson.name}`);
           });
-          setPersons(newPersonsArray);
-          setNewName("");
-          setNewNumber("");
-          notification("success", `Updated ${updatedPerson.name}`);
-        })
       }
       return;
     }
 
-    personsService.create({ name: newName, number: newNumber }).then((newPerson) => {
-      setPersons([...persons, newPerson]);
-      setNewName("");
-      setNewNumber("");  
-      notification("success", `Added ${newPerson.name}`);
-    })
-  }
+    personsService
+      .create({ name: newName, number: newNumber })
+      .then((newPerson) => {
+        setPersons([...persons, newPerson]);
+        setNewName("");
+        setNewNumber("");
+        notification("success", `Added ${newPerson.name}`);
+      })
+      .catch((error) => {
+        notification("error", error.response.data.error);
+      });
+  };
 
   const removePerson = (id) => {
     const selectedPerson = persons.find((person) => person.id === id);
-    if(!window.confirm(`Delete ${selectedPerson.name}?`)) return;
-    personsService.remove(id).then(() => {
-      setPersons(persons.filter((person) => person.id !== id));
-    }).catch((error) => {
-      notification("error", `Information of ${selectedPerson.name} has already been removed from server`);
-      setPersons(persons.filter((person) => person.id !== id))
-    })
-  }
+    if (!window.confirm(`Delete ${selectedPerson.name}?`)) return;
+    personsService
+      .remove(id)
+      .then(() => {
+        setPersons(persons.filter((person) => person.id !== id));
+      })
+      .catch((error) => {
+        notification(
+          "error",
+          `Information of ${selectedPerson.name} has already been removed from server`
+        );
+        setPersons(persons.filter((person) => person.id !== id));
+      });
+  };
 
-  const filteredList = filter.length > 0
-    ? persons.filter((person) => person.name.toLowerCase().includes(filter.toLowerCase()))
-    : persons;
+  const filteredList =
+    filter.length > 0
+      ? persons.filter((person) =>
+          person.name.toLowerCase().includes(filter.toLowerCase())
+        )
+      : persons;
 
   return (
     <div>
@@ -95,7 +117,7 @@ const App = () => {
       <h2>Numbers</h2>
       <Persons persons={filteredList} removePerson={removePerson} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
