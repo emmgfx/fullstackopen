8f5ec196a3f8ec27dfa4b807cdb5be6f9a671abe
Josep Viciana <emmgfx@gmail.com>
2025-05-24 22:34:08 +0200

    part4, exercise 6

--- part4/utils/list_helpers.js
+++ part4/utils/list_helpers.js
@@ -16,8 +16,29 @@ const favoriteBlog = (blogs) => {
   return sortedBlogs[0] || null;
 };
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  const authors = [];
+
+  blogs.forEach((blog) => {
+    const author = blog.author;
+    const authorExists = authors.find((a) => a.author === author);
+    if (authorExists) {
+      authorExists.blogs += 1;
+    } else {
+      authors.push({ author, blogs: 1 });
+    }
+  });
+
+  return authors.sort((a, b) =>
+    a.blogs > b.blogs ? -1 : a.blogs < b.blogs ? 1 : 0
+  )[0];
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
